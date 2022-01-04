8c71636691e59690a89cfad6e359d58c69241661
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-01-04 15:10:57 +0700

    feat(config): enable css optimization with critters
    https://twitter.com/hdjirdeh/status/1369709676271726599?s=20

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+  },
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin());
     return config;
