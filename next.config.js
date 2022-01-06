53b06144d96732dc9bac602ee5faa9edc66d3a4f
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-01-07 01:00:50 +0700

    chore: use preact for production build (#1)

--- next.config.js
+++ next.config.js
@@ -18,8 +18,17 @@ module.exports = withPWA({
   experimental: {
     optimizeCss: true,
   },
-  webpack(config) {
+  webpack(config, { dev, isServer }) {
     config.plugins.push(new WindiCSSWebpackPlugin());
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
     return config;
   },
   images: {
